a15d063e1a65d8bbe5687b1f44e961b57b2843bd
Juan Je Garcia <juanjegarcialopez@gmail.com>
2022-02-10 00:42:01 -0600

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { SearchBookInput } from "./components/searchBookInput";
+import { SearchInput } from "./components/searchInput";
+
+function App() {
+	return (
+		<div className="App">
+			<SearchBookInput />
+		</div>
+	);
+}
+
+export default App;
